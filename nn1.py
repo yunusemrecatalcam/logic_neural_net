08840e28335bd544379cedca257cbf5f9f482633
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2018-08-07 19:59:21 +0300

    completed my first one hidden layer network,vectorizing correctly was the hardest part lol

--- nn1.py
+++ nn1.py
@@ -21,7 +21,7 @@ W2 = np.random.random((1,5))
 Y = np.array([[0],[1],[1],[1]]).T
 
 
-for i in range(1,2):
+for i in range(1,10000):
 
     Z1 = np.dot(W1,X)+b1  #Forward Propagation
     A1 = sigmoid(Z1)
@@ -32,31 +32,28 @@ for i in range(1,2):
     dZ2 = (A2-Y)          #Backpropagation
     dW2 = np.dot(dZ2,A1.T)/4
     db2 = np.sum(dZ2,axis=1,keepdims=True)/4
-    print dW2
-    print("dz2",dZ2)
-    print ("db2",db2)
-    dZ1 = np.dot(dW2.T,dZ2.T)
-    dW1 = np.dot(dZ1,X)
+
+
+    dZ1 = np.dot(dW2.T,dZ2)*sigmoid(Z1,deriv=True)
+    dW1 = np.dot(dZ1,X.T)
     db1 = np.sum(dZ1,axis=1,keepdims=True)/4
-    print("dz1",dZ1)
-    print("db1111",db1)
-    W2 = W2 - dW2.T
-    W1 = W1 - dW1.T
+
+    W2 = W2 - dW2
+    W1 = W1 - dW1
     b2 = b2 - db2
     b1 = b1 - db1
 
-    print(db2*4)
+    print("error:",db2*4)
 
-X_in = np.array([0,0,0])          # This part is for taking inputs for calculating results
-print("w1",W1)
-print("w2",W2)
+X_in = np.array([[0,0,0]]).T          # This part is for taking inputs for calculating results
+
 while 1:
-    X_in[0]=input("input 1")
-    X_in[1]=input("input 2")
-    X_in[2]=input("input 3")
-    Z1 = np.dot(X_in,W1) +b1#Forward Propagation
+    X_in[0]=input("input 1:")
+    X_in[1]=input("input 2:")
+    X_in[2]=input("input 3:")
+    Z1 = np.dot(W1,X_in) +b1#Forward Propagation
     A1 = sigmoid(Z1)
-    print("a1",A1)
-    Z2 = np.dot(A1,W2)+b2
+
+    Z2 = np.dot(W2,A1)+b2
     A2 = sigmoid(Z2)
     print("result:",A2)
