63fc95c41eca8b67da18c0e4a38df004c44debf0
yunusemrecatalcam <yunusemrecatalcam@gmail.com>
2018-08-08 00:12:45 +0300

    nn1 nice trained

--- nn0.py
+++ nn0.py
@@ -4,7 +4,7 @@ LEARNING_RATE = 0.1
 
 def sigmoid(x,deriv=False):
     if (deriv==True):
-        return sigmoid(x)*sigmoid(1-x)
+        return sigmoid(x)*(1-sigmoid(x))
     return (1/(1+np.exp(-x)))
 
 X = np.array([#1 #2
@@ -15,22 +15,22 @@ X = np.array([#1 #2
 b = np.random.random((1))
 W = np.random.random((2))
 Y = np.array([0,1,1,1])
-print (W.shape)
-print(Y.shape)
-for i in range(1,4000):
+
+for i in range(1,2000):
 
     Z = np.dot(X,W)+b   #Forward Propagation
     A = sigmoid(Z)
 
     dZ = (A-Y)          #Backpropagation
-    dW = np.dot(dZ,A.T)/4
+
+    dW = np.dot(dZ,X)/4
     db = np.sum(dZ,axis=0,keepdims=True)/4
 
     print("error:",np.sum(dZ,axis=0,keepdims=True))
     W = W - dW*LEARNING_RATE
     b = b - db*LEARNING_RATE
 
-X_in = np.array([0,0])          # This part is for taking inputs for calculating results
+X_in = np.array([0,0])          # This part is for taking inputs for calculating res
 
 while 1:
     X_in[0]=input("input 1")
